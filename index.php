870581a553766db4ca129aa4c67b8e0d114bc7e4
lucas <garaglialucas@gmail.com>
2023-08-17 14:02:06 -0300

    Modified index.php --

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>git1 v3</title>
+    <title>git1 v4, GITHUB</title>
 </head>
 <body>
     <h1>Hola git1!</h1>
